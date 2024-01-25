c00227a20dfc1a375fcedba41e0a75f13b05187e
BandiBabuReddy <129394364+BandiBabuReddy@users.noreply.github.com>
2024-01-25 12:47:37 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 
-#variable "terraforms3bucket1999" {
+#variable "terraforms3bucket19999" {
 # description = "terraform_s3_bucket"
-# default     = "terraforms3bucket1999" # Change this to your desired S3 bucket name
+# default     = "terraforms3bucket19999" # Change this to your desired S3 bucket name
 #}
