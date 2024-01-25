80d1c7a1360087ed061e992bc0d26acf236b8f85
BandiBabuReddy <129394364+BandiBabuReddy@users.noreply.github.com>
2024-01-25 12:54:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "APP1" {
-  ami           = "ami-008677ef1baf82eaf"
+  ami           = "ami-0a3c3a20c09d6f377"
   instance_type = "t2.micro"
   user_data     = file("${path.module}/App-install.sh") # file provisioners 
   tags = {
